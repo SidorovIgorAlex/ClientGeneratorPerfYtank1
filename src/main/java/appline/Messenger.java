3c6ebb41c0cd19007cd19c3a4cfce90e405bd3fe
AlLipatov <allipatov@yotateam.com>
2021-02-05 15:06:08 +0300

    init commit to UI

--- src/main/java/appline/Messenger.java
+++ src/main/java/appline/Messenger.java
@@ -1,5 +1,6 @@
 package appline;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,7 +8,6 @@ import org.springframework.ui.Model;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,20 +20,21 @@ public class Messenger {
     }
 
     @GetMapping("/generation")
-    public ModelAndView massGenerationGet(Model model) {
-        ModelAndView modelAndView = new ModelAndView("generateUsers");
+    public String massGenerationGet(Model model) {
+        ModelAndView modelAndView = new ModelAndView("generateUsers.mustache");
         modelAndView.addObject("generateUsers", new GenerateUsers());
-        return modelAndView;
+        return "generateUsers";
     }
 
-    @PostMapping("/generation")
-    public Map<String, Object> massGeneratorPost(GenerateUsers generateUsers, HttpServletResponse response) throws IOException {
+    @RequestMapping(value = "/generationUser", method = RequestMethod.POST)
+    public ResponseEntity massGeneratorPost(@RequestBody GenerateUsers generateUsers) throws IOException {
         String nameKey = generateUsers.getNameKey();
         Integer amountUsers = generateUsers.getAmountUsers();
-        response.setContentType("application/json");
         Map<String, Object> json = Client.sendCommandToGenerator(String.format("http://perf-ytank1:8080/generator?type=D2C_AY&totalCount=%d&concurrency=4&redisType=list&redisOptionalKey=%s",
                 amountUsers, nameKey));
-        return json;
+        HashMap<String, Object> resultMap = new HashMap<>();
+        resultMap.put("result",json.toString());
+        return ResponseEntity.ok().body(resultMap);
     }
 
     @GetMapping("/receiveSimCards")
@@ -45,20 +46,20 @@ public class Messenger {
     }
 
     @PostMapping("/receiveSimCards")
-    public Map<String, Object> receiveSimCardsPost(GenerateUsers generateUsers, HttpServletResponse response) throws IOException {
+    public Map<String, Object> receiveSimCardsPost(@RequestBody GenerateUsers generateUsers) throws IOException {
         Integer amountVacantSim = generateUsers.getAmountVacantSim();
         System.out.println(amountVacantSim);
-        response.setContentType("application/json");
         Map<String, Object> json = Client.sendCommandToGenerator(String.format("http://perf-ytank1:8080/vacantSim/phone/upload?limit=%d",
                 amountVacantSim));
         return json;
     }
 
-    @GetMapping("/status")
-    public Map<String, Object> getStatus(HttpServletResponse response) throws IOException {
-        response.setContentType("application/json");
+    @RequestMapping(value = "/status", method = RequestMethod.GET)
+    public ResponseEntity getStatus() throws IOException {
         Map<String, Object> json = Client.sendCommandToGenerator("http://perf-ytank1:8080/genstatus");
-        return json;
+        HashMap<String, Object> resultMap = new HashMap<>();
+        resultMap.put("result",json.toString());
+        return ResponseEntity.ok().body(resultMap);
     }
 
     @GetMapping("/stopAll")
---
+++ src/main/resources/scripts/scripts.js
@@ -0,0 +1,5 @@
+async function generateUser() {
+    let nameKey = document.getElementById("nameKey").getAttribute("value");
+    let amountUsers = document.getElementById("amountUsers").getAttribute("value");
+    alert(nameKey + '' + amountUsers)
+}
